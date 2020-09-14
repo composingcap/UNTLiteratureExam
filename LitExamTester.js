9224fee04f7bfd2de0a0defb46b9c66156f5db7d
composingcap@gmail.com <composingcap@gmail.com>
2020-09-14 08:47:40 -0500

    new test button

--- LitExamTester.js
+++ LitExamTester.js
@@ -274,4 +274,31 @@ function insertParam(key, value) {
     // reload page with new params
     document.location.search = params;
 }
+function newTest(){
+    var newUrl = updateURLParameter(window.location.href,"seed",Math.floor(Math.random()*50000)); 
+    window.location.href = newUrl;
+
+    
+}
+    
+    function updateURLParameter(url, param, paramVal){
+    var newAdditionalURL = "";
+    var tempArray = url.split("?");
+    var baseURL = tempArray[0];
+    var additionalURL = tempArray[1];
+    var temp = "";
+    if (additionalURL) {
+        tempArray = additionalURL.split("&");
+        for (var i=0; i<tempArray.length; i++){
+            if(tempArray[i].split('=')[0] != param){
+                newAdditionalURL += temp + tempArray[i];
+                temp = "&";
+            }
+        }
+    }
+
+    var rows_txt = temp + "" + param + "=" + paramVal;
+    return baseURL + "?" + newAdditionalURL + rows_txt;
+}
+    
 window.addEventListener('DOMContentLoaded', init);
